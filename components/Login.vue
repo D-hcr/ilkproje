21cd44c3a35a8c59949038bc1ac07fa75ab96b37
D-hcr <hcrdz19@gmail.com>
2022-12-01 22:31:44 +0300

    2. navbar tamamlandı ve login düzenlendi

---
+++ components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>        
+    <div id="mySidenav" class="sidenav">
+      <a id="kname" href="javascript:void(0)" class="closebtn" v-on:click="closeNav()">&times; 
+        <span id="k">Kapat</span>      
+      </a>
+      <h1 id="o">Oturum Aç</h1>    
+    </div>    
+    <div>
+      <span id="sp" style="font-size:30px;cursor:pointer" v-on:click="openNav()">&#9776; open</span>
+    </div>  
+  </div>
+</template>
+
+<script lang="js">
+export default {
+     methods: {
+      openNav: function(){
+        document.getElementById("mySidenav").style.width = "500px";        
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+     },
+     closeNav: function(){
+      document.getElementById("mySidenav").style.width = "0";
+      document.body.style.backgroundColor = "white";
+     }
+     },
+};
+</script>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
    <header>
-    <Navbar/>
-    <Katagoriler/>
+      <!-- <Navbar/>
+      <NuxtLogo/> -->
+      <Login/>
    </header>
+   
+  
  
 </template>
 
@@ -13,4 +16,7 @@
 @import "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css";   
 @import "https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css";
 @import url("static/css/navbar.css");
+@import url("static/css/login.css");
+
+
 </style>
