f7b930629b703266fe60ca6434fef188305de582
huseyin doğdu <huso180101@gmail.com>
2022-12-04 11:35:43 +0300

    product details eklendi

--- components/ProductDetails.vue
+++ components/ProductDetails.vue
@@ -6,20 +6,29 @@
        <Katagoriler/>
     </header>
     <body>
-        <div class="card mb-3" style="max-width: 2000px; 
-         border-bottom: none;
+        <div id="cardPr" class="card mb-3" style="max-width: 1000px;
+        border: none;     
         ">
-  <div class="row g-0">
-    <div class="col-md-4">
+  <div   class="row g-0">
+    <div  class="col-md-4">
       <img src="https://cdn.shopify.com/s/files/1/0485/5264/0672/products/71549_1_9a2d7ac0-ce12-4025-a454-89b2ea793a9d_360x.jpg?v=1669968499" class="img-fluid rounded-start"
       
       alt="...">
     </div>
     <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">Aqua di Polo Unisex Mavi Işık Koruyucu Gözlük APSBL01101</h5>
+        <div><a id="as" href="http://localhost:3000">AnaSayfa</a></div>
+        <h5 id="namep" class="card-title">Aqua di Polo Unisex Mavi Işık Koruyucu Gözlük APSBL01101</h5>
         <p id="price">499.00</p>
-            <em >TL</em>
+            <em id="price" >TL</em>
+            <div class="quantity">
+              <div class="qty-group">
+                <input id="qg" type="number" data-qtt-id="quantity__7265323450528" name="quantity" value="1" data-gtm-form-interact-field-id="6">
+              </div>
+              <input data-btn-addtocart="" type="submit" name="add" class="btn" id="product-add-to-cart" value="Sepete ekle" data-form-id="#add-to-cart-form">
+             
+            </div>
+           
       </div>
     </div>
   </div>
@@ -27,3 +36,39 @@
     </body>
   </div>
  </template>
+
+ <style>
+ #namep {
+  font-weight: bold;
+ }
+ #as {
+  position: relative;
+  left: -0px;
+ }
+ #cardPr {
+  position: relative;
+  left: 180px;
+ }
+#price {
+  font-weight: bolder;
+}
+.quantity {
+  position: relative;
+  top: 50px;
+  left: -70px;
+}
+#qg {
+  width: 50px;
+ 
+}
+.btn {
+  background-color: #293355;
+  color: white;
+  position: relative;
+  left: 80px;
+  top: -35px;
+}
+#btsearch {
+  background-color: white;
+}
+</style>
--- components/ProductSlider.vue
+++ components/ProductSlider.vue
@@ -7,10 +7,139 @@
                 </span>
                         
     </span>
-   
-
-   
+    <div  id="carouselExampleControls"  class="carousel slide"  data-bs-ride="carousel" >
+  <div class="carousel-inner" >
+    <div class="carousel-item active">
+        <div  id="pcorusel" class="card" style="width: 15rem; height: 23rem; ">
+            <span id="rid">
+                <img id="resim" src="https://cdn.shopify.com/s/files/1/0485/5264/0672/products/Aqua-di-Polo-Kadin-Gumus-Metal-Kordonlu-Yildiz-Desenli-Dijital-Retro-Saat-APWA063800.jpg?v=1653899466" class="card-img-top" alt="...">
+            </span>
+            <div class="card-body">
+                <p class="card-text">Aqua di Polo  Rose Gold ve Gümüş Metal Kordonlu Dijital Retro KolSaati 2'li Hediye Seti</p>
+                <p id="price">499.00 TL</p>
+            </div>
+       </div>
+       <div  id="pcorusel" class="card" style="width: 15rem; height: 23rem;">
+            <span id="rid">
+                <img id="resim" src="https://cdn.shopify.com/s/files/1/0485/5264/0672/products/Aqua-di-Polo-Unisex-Siyah-Metal-Kordonlu-Mermer-Desenli-Dijital-Retro-Saat-APWA064300.jpg?v=1653899094" class="card-img-top" alt="...">
+            </span>
+            <div class="card-body">
+                <p class="card-text">Aqua di Polo Unisex Siyah Metal Kordonlu Mermer Desenli Dijital Retro Saat </p>
+                <p id="price">499.00 TL</p>
+            </div>
+       </div>
+       <div  id="pcorusel" class="card" style="width: 15rem; height: 23rem;">
+            <span id="rid">
+                <img id="resim" src="https://cdn.shopify.com/s/files/1/0485/5264/0672/products/Aqua-di-Polo-Kadin-Rose-Gold-Metal-Kordonlu-Cicek-Desenli-Dijital-Retro-Saat-APWA063801.jpg?v=1653899814" class="card-img-top" alt="...">
+            </span>
+            <div class="card-body">
+                <p class="card-text">Aqua di Polo Kadın Rose Gold Metal Kordonlu Çiçek Desenli Dijital Retro Saat</p>
+                <p id="price">499.00 TL</p>
+            </div>
+       </div>
+       <div  id="pcorusel" class="card" style="width: 15rem; height: 23rem;">
+            <span id="rid">
+                <img id="resim" src="https://cdn.shopify.com/s/files/1/0485/5264/0672/products/Aqua-di-Polo-Kadin-Altin-Metal-Kordonlu-Kurukafa-Desenli-Dijital-Retro-Saat-APWA063400.jpg?v=1653899219" class="card-img-top" alt="...">
+            </span>
+            <div class="card-body">
+                <p class="card-text">Aqua di Polo Kadın Altın Metal Kordonlu Kurukafa Desenli Dijital Retro Saat</p>
+                <p id="price">499.00 TL</p>
+            </div>
+       </div>
+    </div>
+    <div class="carousel-item">
+        <div  id="pcorusel" class="card" style="width: 15rem; height: 23rem;">
+            <span id="rid">
+                <img id="resim" src="https://cdn.shopify.com/s/files/1/0485/5264/0672/products/Aqua-di-Polo-Gran-Paradiso-Sport-50-Ml-EDP-Erkek-Parfum-APCN0005.jpg?v=1649181678" class="card-img-top" alt="...">
+            </span>
+            <div class="card-body">
+                <p class="card-text">Aqua di Polo Gran Paradiso Sport 50 Ml EDP Erkek Parfüm </p>
+                <p id="price">499.00 TL</p>
+            </div>
+       </div>
+       <div  id="pcorusel" class="card" style="width: 15rem; height: 23rem;">
+            <span id="rid">
+                <img id="resim" src="https://cdn.shopify.com/s/files/1/0485/5264/0672/products/Aqua-di-Polo-Gran-Paradiso-Red-50-Ml-EDP-Erkek-Parfum-APPPGR03EP.jpg?v=1649181282" class="card-img-top" alt="...">
+            </span>
+            <div class="card-body">
+                <p class="card-text">Aqua di Polo Gran Paradiso Red 50 Ml EDP Erkek Parfüm</p>
+                <p id="price">499.00 TL</p>
+            </div>
+       </div>
+       <div  id="pcorusel" class="card" style="width: 15rem; height: 23rem;">
+            <span id="rid">
+                <img id="resim" src="https://cdn.shopify.com/s/files/1/0485/5264/0672/products/Aqua-di-Polo-Gran-Paradiso-Oasis-50-Ml-EDP-Erkek-Parfum-APCN000504.jpg?v=1649187210" class="card-img-top" alt="...">
+            </span>
+            <div class="card-body">
+                <p class="card-text">Aqua di Polo Gran Paradiso Oasis 50 Ml EDP Erkek Parfüm </p>
+                <p id="price">499.00 TL</p>
+            </div>
+       </div>
+       <div  id="pcorusel" class="card" style="width: 15rem; height: 23rem;">
+            <span id="rid">
+                <img id="resim" src="https://cdn.shopify.com/s/files/1/0485/5264/0672/products/Aqua-di-Polo-Gran-Paradiso-Volcano-Sport-50-Ml-EDP-Erkek-Parfum-APCN000512.jpg?v=1649181488" class="card-img-top" alt="...">
+            </span>
+            <div class="card-body">
+                <p class="card-text">Aqua di Polo Gran Paradiso Volcano Sport 50 Ml EDP Erkek Parfüm</p>
+                <p id="price">499.00 TL</p>
+            </div>
+       </div>
+    </div>
+    <div class="carousel-item">
+        <div  id="pcorusel" class="card" style="width: 15rem; height: 23rem;">
+            <span id="rid">
+                <img id="resim" src="https://cdn.shopify.com/s/files/1/0485/5264/0672/products/Aqua-di-Polo-Unisex-Mor-Premium-Sirt-Cantasi-APBA012805_360x.png?v=1649220798" class="card-img-top" alt="...">
+            </span>
+            <div class="card-body">
+                <p class="card-text">Aqua di Polo Unisex Mor Premium Sırt Çantası</p>
+                <p id="price">499.00 TL</p>
+            </div>
+       </div>
+       <div  id="pcorusel" class="card" style="width: 15rem; height: 23rem;">
+            <span id="rid">
+                <img id="resim" src="https://cdn.shopify.com/s/files/1/0485/5264/0672/products/71965_1_360x.jpg?v=1667403707" class="card-img-top" alt="...">
+            </span>
+            <div class="card-body">
+                <p class="card-text">Aqua di Polo 1987 APBA044001 Kadın Bej Omuz Çantası</p>
+                <p id="price">499.00 TL</p>
+            </div>
+       </div>
+       <div  id="pcorusel" class="card" style="width: 15rem; height: 23rem;">
+            <span id="rid">
+                <img id="resim" src="https://cdn.shopify.com/s/files/1/0485/5264/0672/products/Aqua-di-Polo-Kadin-Siyah-Omuz-Askili-Telefon-Cantasi-APBA041001_360x.jpg?v=1652862742" class="card-img-top" alt="...">
+            </span>
+            <div class="card-body">
+                <p class="card-text">Aqua di Polo Kadın Siyah Omuz Askılı Telefon Çantası</p>
+                <p id="price">499.00 TL</p>
+            </div>
+       </div>
+       <div  id="pcorusel" class="card" style="width: 15rem; height: 23rem;">
+            <span id="rid">
+                <img id="resim" src="https://cdn.shopify.com/s/files/1/0485/5264/0672/products/Aqua-di-Polo-Kadin-Krem-Omuz-Cantasi-APBA014004_360x.jpg?v=1650373610" class="card-img-top" alt="...">
+            </span>
+            <div class="card-body">
+                <p class="card-text">Aqua di Polo Kadın Krem Omuz Çantası</p>
+                <p id="price" >499.00 TL</p>
+            </div>
+       </div>
+    </div>
+  </div>
+  <button id="button1" class="carousel-control-prev" type="button" style="color:black" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span  class="carousel-control-prev-icon" style="color:black" aria-hidden="true"></span>
+    <span id="iconx" class="visually-hidden">Previous</span>
+  </button>
+  <button id="button2" class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span  class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span id="iconx" class="visually-hidden">Next</span>
+  </button>
 </div>
+<a id="tg" href="">
+    <p id="ttg">
+        Tümünü Gör
+    </p>
+  </a>
+</div>
+ 
 </template>
 
 <style lang="scss">
@@ -24,9 +153,69 @@
 .title {
     margin-top: 47px;
 }
+#carouselExampleControls{
+    position: relative;
+    left: 250px;
+    top: 50px;
+    height: 400px;
+    width: 1100px;
+}
+#resim {
+    height: 200px;
+    width: 150px;
+    position: relative;
+    left: 40px;
+   
+}
+#button1 {
+    background-color: rgb(204, 203, 203);
+    height: 50px;
+    width: 50px;
+    position: absolute;
+    left: -60px;
+    top: 130px;
+    
+}
+#button2 {
+    background-color: rgb(204, 203, 203);
+    height: 50px;
+    width: 50px;
+    position: absolute;
+    right: -60px;
+    top: 120px;
+    
+}
+#rid {
+    border: solid rgb(200, 198, 198);
+    position: relative;
+    left: -1px;
 
-
-
+}
+#pcorusel {
+    border: none;
+    float: left;
+    margin-left: 25px;
+    height: 300px;
+    
+}
+#price {
+    font-weight:bold;
+}
+#tg {
+    border:solid;
+    position: relative;
+    top: 60px;
+    left: 660px;
+    width: 250px;
+    color: rgb(135, 132, 132);
+}
+#ttg {
+    position: relative;
+    left: 70px;
+    top: 10px;
+    text-decoration-line: none;
+    color: black;
+}
 </style>
 
 <script>
